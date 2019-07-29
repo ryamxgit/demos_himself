f696a215cdba6e9bb2e6f4fafcc2c1d8db76466d
Rodrigo Guevara <rguevara@lapolar.cl>
2019-07-29 16:17:33 -0400

    Se agrega texto de prueba

--- sudoku/sudoku_drawing.js
+++ sudoku/sudoku_drawing.js
@@ -1,27 +1,39 @@
 
-var drawGridLines = function(num_rectangles_wide, num_rectangles_tall, boundingRect) {
-    var width_per_rectangle = boundingRect.width / num_rectangles_wide;
-    var height_per_rectangle = boundingRect.height / num_rectangles_tall;
-    for (var i = 0; i <= num_rectangles_wide; i++) {
-        var xPos = boundingRect.left + i * width_per_rectangle;
-        var topPoint = new paper.Point(xPos, boundingRect.top);
-        var bottomPoint = new paper.Point(xPos, boundingRect.bottom);
-        var aLine = new paper.Path.Line(topPoint, bottomPoint);
-        aLine.strokeColor = 'black';
-    }
-    for (var i = 0; i <= num_rectangles_tall; i++) {
-        var yPos = boundingRect.top + i * height_per_rectangle;
-        var leftPoint = new paper.Point(boundingRect.left, yPos);
-        var rightPoint = new paper.Point(boundingRect.right, yPos);
-        var aLine = new paper.Path.Line(leftPoint, rightPoint);
-        aLine.strokeColor = 'black';
-    }
+var Sudoku = function() {
+	var width_per_rectangle = 0;
+	var height_per_rectangle = 0;
+	var drawGridLines = function(num_rectangles_wide, num_rectangles_tall, boundingRect) {
+		this.width_per_rectangle = boundingRect.width / num_rectangles_wide;
+		this.height_per_rectangle = boundingRect.height / num_rectangles_tall;
+		for (var i = 0; i <= num_rectangles_wide; i++) {
+			var xPos = boundingRect.left + i * this.width_per_rectangle;
+			var topPoint = new paper.Point(xPos, boundingRect.top);
+			var bottomPoint = new paper.Point(xPos, boundingRect.bottom);
+			var aLine = new paper.Path.Line(topPoint, bottomPoint);
+			aLine.strokeColor = 'black';
+		}
+		for (var i = 0; i <= num_rectangles_tall; i++) {
+			var yPos = boundingRect.top + i * this.height_per_rectangle;
+			var leftPoint = new paper.Point(boundingRect.left, yPos);
+			var rightPoint = new paper.Point(boundingRect.right, yPos);
+			var aLine = new paper.Path.Line(leftPoint, rightPoint);
+			aLine.strokeColor = 'black';
+		}
+	}
+	var drawNumberTexts = function(num, posx, posy) {
+		var locX = (posx - 1)*this.width_per_rectangle + (this.width_per_rectangle / 2);
+		var locY = (posy - 1)*this.height_per_rectangle + (this.height_per_rectangle / 2);
+		var text = new PointText({content: num, fontSize: 12, justification: 'center', point: newPint(locX, locY)});
+	}
 }
 
 window.onload = function() {
 	var canvas = document.getElementById('sudokuZone');
 	paper.setup(canvas);
+	var su = new Sudoku();
 
-	drawGridLines(9, 9, paper.view.bounds);
+	su.drawGridLines(9, 9, paper.view.bounds);
 	paper.view.draw();
+	
+	su.drawNumberTexts(1,3,4);
 }
