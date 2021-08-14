4e719ed8f2c34b63b8d1beaa42a8aa4947b8df8d
Rodrigo Guevara <rodri.guevara@gmail.com>
2021-08-14 16:15:33 -0400

    Modifica codigo js

--- p5_example/p5_exerc.js
+++ p5_example/p5_exerc.js
@@ -1,4 +1,4 @@
-float angle = 90;
+var angle = 90;
 function setup() {
 	size(900,900);
 }
@@ -8,7 +8,7 @@ function draw() {
 	translate(width/2, height/2);
 	stroke(255);
 	strokeWeight(3);
-	for(int i=0; i<280; i+=10) {
+	for(var i=0; i<280; i+=10) {
 		pushMatrix();
 		rotate(radians(i)*
 				cos(radians(angle)));
