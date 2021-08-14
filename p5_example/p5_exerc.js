7bb01ea6dc6f8bddafe44ae43206b6b893962c86
Rodrigo Guevara <rodri.guevara@gmail.com>
2021-08-14 16:25:30 -0400

    ultima prueba sabado

--- p5_example/p5_exerc.js
+++ p5_example/p5_exerc.js
@@ -1,18 +1,19 @@
 var angle = 90;
 function setup() {
-	createCanvas(900,900);
+	createCanvas(1800,900);
 }
 
 function draw() {
 	background(0);
 	translate(width/2, height/2);
 	stroke(255);
-	strokeWeight(3);
+	strokeWeight(5);
+	var lenght = 400;
 	for(var i=0; i<280; i+=10) {
 		push();
 		rotate(radians(i)*
 				cos(radians(angle)));
-		line(200*sin(radians(angle)),0,0,200);
+		line(length*sin(radians(angle)),0,0,length);
 		pop();
 	}
 	angle++;
