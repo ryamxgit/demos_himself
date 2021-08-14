348744cb1f88c0120335ca4ad4853c4c0749b721
Rodrigo Guevara <rodri.guevara@gmail.com>
2021-08-14 16:27:41 -0400

    ultima prueba ahora si que si

--- p5_example/p5_exerc.js
+++ p5_example/p5_exerc.js
@@ -7,13 +7,13 @@ function draw() {
 	background(0);
 	translate(width/2, height/2);
 	stroke(255);
-	strokeWeight(5);
-	var lenght = 300;
+	strokeWeight(3);
+	var lenght = 400;
 	for(var i=0; i<280; i+=10) {
 		push();
 		rotate(radians(i)*
 				cos(radians(angle)));
-		line(200*sin(radians(angle)),0,0,length);
+		line(length*sin(radians(angle)),0,0,200);
 		pop();
 	}
 	angle++;
