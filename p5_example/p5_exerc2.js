7b9bb48860f35315f1507160e770fc010a12deb9
Rodrigo G <rguevara@lapolar.cl>
2024-06-29 19:58:36 -0400

    resolution calculated

--- p5_example/p5_exerc2.js
+++ p5_example/p5_exerc2.js
@@ -39,7 +39,9 @@ const FRAGMENT_SOURCE = [
 
 
 function setup() {
-	createCanvas(1280,720, WEBGL);
+	let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+	let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+	createCanvas(vw,vh, WEBGL);
 	noStroke();
 	background(0);
   
