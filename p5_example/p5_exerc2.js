d643149a940cd1e24422dae625767a4109cd39a9
Rodrigo G <rguevara@lapolar.cl>
2024-06-29 19:39:21 -0400

    Cambia de import file to insert code

--- p5_example/p5_exerc2.js
+++ p5_example/p5_exerc2.js
@@ -1,10 +1,49 @@
+const VERTEX_SOURCE = [
+  '#version 100',
+  'precision mediump float;',
+  'attribute vec3 aPosition;',
+
+  'void main(void) {',
+    'gl_Position = vec4(aPosition, 1.0);',
+  '}',
+].join('\n');
+
+const FRAGMENT_SOURCE = [
+  '#version 100',
+  'precision mediump float;',
+  
+  'uniform vec2 resolution;',
+  'uniform float time;',
+  
+  'const int MaxIter = 100;',
+  'const float MaxZ = 16.0;',
+
+  'void main(void) {',
+    'vec2 uv = (gl_FragCoord.xy * 2.0 - resolution.xy) / min(resolution.x, resolution.y);',
+    'vec2 c=vec2(sin(time * 0.1) + cos(time * 0.23), cos(time * 0.13) + sin(time * 0.21))*0.5;',
+    'vec2 z = uv;',
+    'int cIter=MaxIter;',
+    'for (int iter=0; iter<MaxIter; iter++) {',
+      'if (dot(z, z)>=MaxZ) { cIter=iter; break; }',
+      'vec2 z2=vec2(z.x * z.x - z.y * z.y, 2. * z.x * z.y);',
+      'z = z2 + c;',
+    '}',
+    
+    'if (cIter>=MaxIter) {',
+      'gl_FragColor = vec4(1., 1., 0., 1.);',
+    '} else {',
+      'gl_FragColor = vec4(0., .3, .4, 1.) * (float(cIter) / 10.);',
+    '}',
+  '}',
+].join('\n');
+
 
 function setup() {
 	createCanvas(1800,900, WEBGL);
 	noStroke();
 	background(0);
   
-	shaderProg = createShader("/p5_example/vertex_file.js", "/p5_example/segment_file.js");
+	shaderProg = createShader(VERTEX_SOURCE, FRAGMENT_SOURCE);
 }
 
 function draw() {
