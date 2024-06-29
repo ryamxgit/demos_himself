d0b939d7fdbdd15a044f8bc9a7e898ce36e6e6f5
Rodrigo G <rguevara@lapolar.cl>
2024-06-29 15:59:14 -0400

    Agrega nuevo ejercicio

---
+++ p5_example/p5_exerc2.js
@@ -0,0 +1,18 @@
+var angle = 90;
+function setup() {
+	createCanvas(1800,900);
+}
+
+function draw() {
+	float i, e, R, s;
+	vec3 q, p, d = vec3((r - FC.xy * (3. - sin(t * .5))) / r.y, 2);
+	for (q.zy--; i++ < 89.;) {
+		e += i / 4e3;
+		o.rgb += hsv(.1, e, R * i * e / 179.);
+		s = 1.1;
+		p = q += d * e * R * .06;
+		p = vec3(log2(R = length(p)) - t * .5, exp(-p.z / R), atan(p.y, p.x));
+		for (e = --p.y; s < 8e2; s += s)
+			e += .1 - abs(dot(cos(p.zxy * s), cos(p * s)) / s * .8);
+	}
+}
